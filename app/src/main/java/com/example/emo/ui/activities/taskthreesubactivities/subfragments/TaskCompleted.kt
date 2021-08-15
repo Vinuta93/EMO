a8418891f445b08c8cd9c9ead795036976966bfb
Vinuta <75907012+Vinuta93@users.noreply.github.com>
2021-08-15 21:25:02 +0100

    Minor FIxes

--- app/src/main/java/com/example/emo/ui/activities/taskthreesubactivities/subfragments/TaskCompleted.kt
+++ app/src/main/java/com/example/emo/ui/activities/taskthreesubactivities/subfragments/TaskCompleted.kt
@@ -3,13 +3,21 @@ package com.example.emo.ui.activities.taskthreesubactivities.subfragments
 import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.widget.Button
 import com.example.emo.Activities
 import com.example.emo.R
 
 class TaskCompleted : AppCompatActivity() {
+
+    lateinit var btn : Button
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_task_completed)
+        btn = findViewById(R.id.btn)
+        btn.setOnClickListener{
+            startActivity(Intent(this, Activities::class.java))
+        }
     }
 
     override fun onBackPressed() {
